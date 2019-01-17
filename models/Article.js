21403e59e99f1c9f1f34c1bb40c33dbf58b4d832
catmblake <catmblake@gmail.com>
2019-01-17 14:42:42 -0800

    added basic article model

---
+++ models/Article.js
@@ -0,0 +1,30 @@
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+// New Article schema
+var ArticleSchema = new Schema({
+  // title: type string, required
+  title: {
+    type: String,
+    required: true
+  },
+  // link: type: string, required
+  link: {
+    type: String,
+    required: true
+  },
+  // note is an object that stores a Note id
+  // ref links the ObjectId of Article to the associated note
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
+});
+
+// This creates our model from the above schema, using mongoose's model method
+var Article = mongoose.model("Article", ArticleSchema);
+
+// Export the Article model
+module.exports = Article;
