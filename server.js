b739e061b25ea14ee504320cf0f6edf1bac7c200
catmblake <catmblake@gmail.com>
2019-01-17 22:11:23 -0800

    scrape route working

--- server.js
+++ server.js
@@ -31,11 +31,51 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Connect to Mongo DB
-mongoose.connect("mongodb://localhost/mongooseInTheNews", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/mongoHeadLines", { useNewUrlParser: true });
 
 app.get("/", function(req, res) {
   res.render("index");
 });
+
+// A GET route for scraping the echoJS website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with axios
+  axios.get("http://www.huffingtonpost.com/").then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+
+    // Now, we grab every article tag, and do the following:
+    $("div.card__content").each(function(i, element) {
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(element)
+        .find("div.card__headline__text")
+        .text();
+      result.link = $(element)
+        .find("a")
+        .attr("href");
+
+      // Create a new Article using the `result` object built from scraping
+      // db.Article.create(result)
+      //   .then(function(dbArticle) {
+      //     // View the added result in the console
+      //     console.log(dbArticle);
+      //   })
+      //   .catch(function(err) {
+      //     // If an error occurred, log it
+      //     console.log(err);
+      //   });
+      console.log(result);
+    });
+
+    // Send a message to the client
+    res.send("Scrape Complete");
+  });
+});
+
+
 
 // Start the server
 app.listen(PORT, function() {
