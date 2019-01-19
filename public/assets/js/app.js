361f6985d3fc9f060cc5ed74948aac95b72f554a
catmblake <catmblake@gmail.com>
2019-01-18 20:07:42 -0800

    added front end event handler for adding notes to articles

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
         alert(err.responseText);
       });
   });
-
+  // On click function to empty all documents from the database
   $("#empty").on("click", function () {
     $.ajax({
       method: "DELETE",
@@ -50,5 +50,21 @@ $(document).ready(function () {
         console.log(err);
         alert(err.responseText);
       });
+  });
+
+  // Click function to save a note to the corresponding article
+  $(".add-note").on("click", function() {
+    var title = $("textarea.title").val().trim();
+    var body = $("textarea.body").val().trim();
+    var artId = $(this).data("article");
+    console.log(title + body + artId);
+
+    // $.ajax({
+    //   method: "POST",
+    //   url: `/articles/${artId}`,
+    //   data: {
+    //     title:
+    //   }
+    // })
   })
 });
